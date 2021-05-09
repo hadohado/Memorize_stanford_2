188c937c9440f667955f9646ea2b2735299af871
hadohado <hadohado@gmail.com>
2021-05-08 19:40:09 -0700

    Initial Commit

---
+++ Memorize_stanford_2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize_stanford_2
+//
+//  Created by ha tuong do on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

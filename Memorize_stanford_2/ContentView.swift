40db6168205379f1386bd855595cdf0b58091c7a
hadohado <hadohado@gmail.com>
2021-05-08 19:43:16 -0700

    ready for start of lecture 2

--- Memorize_stanford_2/ContentView.swift
+++ Memorize_stanford_2/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack {
+            ForEach(0..<4) {index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+    }
+    
+}
+
+struct CardView: View {
+    var isFaceUp:  Bool  // = true
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("😆").padding().font(Font.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
     }
 }
 

192b591bc69340a9d17ced982010c5a1f4bc6cdc
hadohado <hadohado@gmail.com>
2021-05-11 23:36:06 -0700

    add nice font

--- Memorize_stanford_2/EmojiMemoryGameView.swift
+++ Memorize_stanford_2/EmojiMemoryGameView.swift
@@ -28,15 +28,28 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
-                Text(card.content).padding().font(Font.largeTitle)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
-        }
+        // GeometryReader(content: <#T##(GeometryProxy) -> _#>)
+        // container view that defines its content as a function of its own size and coordinate space
+        // view returns a flexible preferred size to its parent layout
+        // init(content: @escaping (GeometryProxy) -> Content)
+        // input argument for content function is GeometryProxy
+        // output of content func is Content, which is a View
+        // @frozen struct GeometryReader<Content> where Content : View
+        
+        GeometryReader { geometry  in
+            ZStack {
+                if card.isFaceUp {
+                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                    Text(card.content)
+                    //Text(card.content).padding().font(Font.largeTitle)
+                } else {
+                    RoundedRectangle(cornerRadius: 10.0).fill()
+                }
+            } // Z
+            .font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.75 ))
+            // .font(Font.largeTitle)
+        } // Geo
     }
 }
 

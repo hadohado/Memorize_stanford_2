d64cc8b0fd1be3e56c2405799b41344fee747813
hadohado <hadohado@gmail.com>
2021-05-14 15:31:07 -0700

    body1()

--- Memorize_stanford_2/Grid.swift
+++ Memorize_stanford_2/Grid.swift
@@ -20,7 +20,6 @@ struct Grid<Item, ItemView> : View where Item: Identifiable, ItemView: View {
     var body: some View {
         GeometryReader { geometry  in
             body1(for:  GridLayout(itemCount: items.count, in: geometry.size))
-                
             }
     }
     
@@ -30,20 +29,9 @@ struct Grid<Item, ItemView> : View where Item: Identifiable, ItemView: View {
             return viewForItem(item)
                 .frame(width: layout.itemSize.width, height: layout.itemSize.height)
                 .position(layout.location(ofItemAt: self.index(of: item)))
-
-            // don't do this: only show 1 card
-            //   viewForItem(item)
-            //   .frame(width: layout.itemSize.width, height: layout.itemSize.height)
         }
     }
     
-    func body2(for item: Item, in layout: GridLayout) -> some View {
-        let index = self.index(of: item)
-        return viewForItem(item)
-            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
-            .position(layout.location(ofItemAt: index))
-    }
-
     func index(of item: Item) -> Int {
         for index in 0..<items.count {
             if items[index].id == item.id { return index}
@@ -51,26 +39,4 @@ struct Grid<Item, ItemView> : View where Item: Identifiable, ItemView: View {
         return 0 // TODO: bogus
     }
     
-    /*
-    func body1(for layout: GridLayout) -> some View {
-        ForEach(items) { item in
-            body2(for: item, in: layout)
-        }
-    }
-    
-    func body2(for item: Item, in layout: GridLayout) -> some View {
-        return viewForItem(item)
-            .frame(width: layout.itemSize.width, height: layout.itemSize.height)
-    }
-    */
 }
-
-
-
-
-
-//struct Grid_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Grid()
-//    }
-//}
